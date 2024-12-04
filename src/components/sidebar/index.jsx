6507259d81d59ef10ad43c29dba2ae6ec3a02dfd
ArturBaghdanyan <arturbaghdanyan20@gmail.com>
2024-12-04 17:12:51 +0400

    added header,aside,sidebar components in MainPage

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from "./style.module.scss";
+import logo from "../../assets/mainpage/header_images/logo.svg";
+import ListPages from './list';
+import { pagesList } from '../../data/pages';
 
 const SideBar = () => {
-  return (
-    <div className={style.sidebar}>
+  const [list, setList] = useState(pagesList);
 
+  return (
+    <div className={`${style.sidebar} ${style.item3}`}>
+      <div className='container_spacing'>
+        <div className={style.sidebar_logo}>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className={style.sidebar_list}>
+          {list.map(i => (
+            <ListPages key={i.id} icon={i.icon} name={i.name} />
+          ))}
+        </div>
+      </div>
+      
     </div>
   )
 }
