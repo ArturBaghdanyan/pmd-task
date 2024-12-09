8fed7a102ccaeff73fa3064f203c8d087d62f755
ArturBaghdanyan <arturbaghdanyan20@gmail.com>
2024-12-09 23:49:04 +0400

    changed style orders list

--- src/pages/orders/index.jsx
+++ src/pages/orders/index.jsx
@@ -15,26 +15,39 @@ const OrdersPage = () => {
         <div className='container_spacing'>
           <h2>Order list</h2>
           <div className={style.orders_list}>
+
             <div className={style.orders_list_column}>
 
             {orderData.map(item => (
-              <div key={item.id} className={style.orders_list_column_list}>
-              <h3>{item.title}</h3>
-              <div className={style.orders_list_column_list_item}>
-                <div className={style.orders_list_column_list_item_text}>
-                  {item.column.text &&
-                    item.column.text.map((text, index) =>
-                    item.column.id === 7 ? ( 
-                      <Link key={`${item.id}-${index}`} to="/orderDetails">
-                        <button key={`${item.id}-${index}`}>{text}</button>
-                      </Link>
-                    ) : (
-                      <span key={`${item.id}-${index}`}>{text}</span>
+              <div key={item.id}>
+                <div className={style.orders_list_column_list}>
+                  {item.title && item.title.map((text, index) => (
+                    <div key={`${item.id}-${index}`} 
+                    className={style.orders_list_column_list_title}
+                    >
+                      <h3>{text}</h3>
+                    </div>
+                  ))}
+                </div>
+                <div className={style.orders_list_column_list_item}>
+                  {item.column &&
+                    item.column.map((i) => (
+                      <div key={`column-${i.id}`}
+                      className={style.orders_list_column_list_item_row}>
+                        <span>{i.text}</span>
+                        <span>{i.orderId}</span>
+                        <span>{i.type}</span>
+                        <span>{i.userId}</span>
+                        <span>{i.date}</span>
+                        <span>{i.amount}</span>
+                        <Link to="/orderDetails">
+                          <button>{i.action}</button>
+                        </Link>
+                      </div>
                     )
                   )}
                 </div>
               </div>
-            </div>
             ))}
             </div>
           </div>
