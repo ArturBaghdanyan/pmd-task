2ef5720176acd61fffa834ff89d741d9e1166acf
ArturBaghdanyan <arturbaghdanyan20@gmail.com>
2024-12-12 23:56:31 +0400

    executed swiper for orders page

--- src/pages/orders/index.jsx
+++ src/pages/orders/index.jsx
@@ -1,9 +1,8 @@
 import React, {useState} from 'react'
 import style from "./style.module.scss";
 import { orderData } from '../../data/orders/orderList';
-// import { SwiperList } from './swiperClick';
+import { SwiperList } from './swiperClick';
 import Header from '../../components/header';
-import { Link } from 'react-router-dom';
 
 const OrdersPage = () => {
   const [currentPage, setCurrentPage] = useState('Orders');
@@ -11,48 +10,15 @@ const OrdersPage = () => {
   return (
     <>
       <Header isHomePage={currentPage} />
+
       <div className={style.orders}>
         <div className='container_spacing'>
           <h2>Order list</h2>
           <div className={style.orders_list}>
-
-            <div className={style.orders_list_column}>
-
-            {orderData.map(item => (
-              <div key={item.id}>
-                <div className={style.orders_list_column_list}>
-                  {item.title && item.title.map((text, index) => (
-                    <div key={`${item.id}-${index}`} 
-                    className={style.orders_list_column_list_title}
-                    >
-                      <h3>{text}</h3>
-                    </div>
-                  ))}
-                </div>
-                <div className={style.orders_list_column_list_item}>
-                  {item.column &&
-                    item.column.map((i) => (
-                      <div key={`column-${i.id}`}
-                      className={style.orders_list_column_list_item_row}>
-                        <span>{i.text}</span>
-                        <span>{i.orderId}</span>
-                        <span>{i.type}</span>
-                        <span>{i.userId}</span>
-                        <span>{i.date}</span>
-                        <span>{i.amount}</span>
-                        <Link to="/orderDetails">
-                          <button>{i.action}</button>
-                        </Link>
-                      </div>
-                    )
-                  )}
-                </div>
-              </div>
-            ))}
-            </div>
+             <SwiperList data={orderData} />  
           </div>
         </div>
-        {/* <SwiperList /> */}
+        
       </div>
     </>
 
--- src/pages/orders/swiperClick.jsx
+++ src/pages/orders/swiperClick.jsx
@@ -1,53 +1,254 @@
-// import { useRef } from 'react';
-// import left from '../../assets/orders-page/left.svg';
-// import right from '../../assets/orders-page/right.svg';
-// import style from "./style.module.scss";
-// import { Swiper, SwiperSlide } from 'swiper/react';
+import { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Link } from 'react-router-dom';
+import 'swiper/css';
+import "swiper/css/pagination";
+import style from "./style.module.scss";
+import left from '../../assets/orders-page/left.svg';
+import right from '../../assets/orders-page/right.svg';
 
-// import 'swiper/css';
-// import "swiper/css/pagination";
+export function SwiperList({ data }) {
+	const [pages, setPages] = useState([1,2,3,4,5])
+	const [isActive, setIsActive] = useState(0)
+	
+  const swiperRef = useRef(null);
 
-// export function SwiperList() {
-//   const swiperRef = useRef(null);
+  const handlePrev = () => {
+    if (swiperRef.current && swiperRef.current.slidePrev) {
+      swiperRef.current.slidePrev();
+    }
+  };
 
-//   const handlePrev = () => {
-//     if (swiperRef.current) swiperRef.current.slidePrev();
-//   };
+  const handleNext = () => {
+    if (swiperRef.current && swiperRef.current.slideNext) {
+      swiperRef.current.slideNext();
+    }
+  };
+	const goToSlide = (index) => {
+    if (swiperRef.current) swiperRef.current.slideTo(index);
+  };
 
-//   const handleNext = () => {
-//     if (swiperRef.current) swiperRef.current.slideNext();
-//   };
+	const buttonStyles = {
+		width: "40px",
+		height: "40px",
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		borderRadius: "10px",
+		color: "rgba(255, 255, 255, 1)",
+		background: "rgba(24, 20, 243, 1)",
+		border: "none"
+	}
 
-
-//   return (
-//     <div className={style.orders_mySwiper}>
-
-//       <button onClick={handlePrev}>
-//         <img src={left} alt="left" />
-//         <span>Previous</span>
-//       </button>
-
-//       <Swiper
-//         spaceBetween={20}
-//         slidesPerView={4}
-//         autoHeight={true}
-//         loop={false}
-//         navigation
-//         pagination
-//         onSwiper={(swiper) => (swiperRef.current = swiper)}
-//         className={style.orders_mySwiper_swiperList}
-//     >
-//       <SwiperSlide style={{width: '40px !important'}}>1</SwiperSlide>
-//       <SwiperSlide style={{width: '40px !important'}}>2</SwiperSlide>
-//       <SwiperSlide style={{width: '40px !important'}}>3</SwiperSlide>
-//       <SwiperSlide style={{width: '40px !important'}}>4</SwiperSlide>
-//     </Swiper>
-
-//       <button onClick={handleNext}>
-//         <span>Next</span>
-//         <img src={right} alt="right" />
-//       </button>
-
-//     </div>
-//   );
-// }
+  return (
+   <>
+    <div className={style.orders_list_column}>
+      <Swiper
+        loop={true}
+        onSwiper={(swiper) => (swiperRef.current = swiper)}
+				onSlideChange={(swiper) => setIsActive(swiper.realIndex)}
+        className={style.orders_list_column_mySwiper}
+      >
+        {data.map((item) => (
+					<div key={item.id}>
+					 	<SwiperSlide className={style.orders_list_column_mySwiper_list}>
+            <div className={style.orders_list_column_mySwiper_list_header}>
+              {item.title &&
+                item.title.map((text, index) => (
+                  <div
+                    key={`${item.id}-${index}`}
+                    className={style.orders_list_column_mySwiper_list_header_title}
+                  >
+                    <h3>{text}</h3>
+                  </div>
+                ))}
+            </div>
+            <div className={style.orders_list_column_mySwiper_list_body}>
+              {item.column &&
+                item.column.map((i) => (
+                  <div
+                    key={`column-${i.id}`}
+                    className={style.orders_list_column_mySwiper_list_body_row}
+                  >
+                    <span>{i.text}</span>
+                    <span>{i.orderId}</span>
+                    <span>{i.type}</span>
+                    <span>{i.userId}</span>
+                    <span>{i.date}</span>
+                    <span>{i.amount}</span>
+                    <Link to="/orderDetails">
+                      <button>{i.action}</button>
+                    </Link>
+                  </div>
+                ))}
+            </div>
+          	</SwiperSlide>
+						<SwiperSlide className={style.orders_list_column_mySwiper_list}>
+							<div className={style.orders_list_column_mySwiper_list_header}>
+								{item.title &&
+									item.title.map((text, index) => (
+										<div
+											key={`${item.id}-${index}`}
+											className={style.orders_list_column_mySwiper_list_header_title}
+										>
+											<h3>{text}</h3>
+										</div>
+									))}
+							</div>
+							<div className={style.orders_list_column_mySwiper_list_body}>
+								{item.column &&
+									item.column.map((i) => (
+										<div
+											key={`column-${i.id}`}
+											className={style.orders_list_column_mySwiper_list_body_row}
+										>
+											<span>{i.text}</span>
+											<span>{i.orderId}</span>
+											<span>{i.type}</span>
+											<span>{i.userId}</span>
+											<span>{i.date}</span>
+											<span>{i.amount}</span>
+											<Link to="/orderDetails">
+												<button>{i.action}</button>
+											</Link>
+										</div>
+									))}
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className={style.orders_list_column_mySwiper_list}>
+							<div className={style.orders_list_column_mySwiper_list_header}>
+								{item.title &&
+									item.title.map((text, index) => (
+										<div
+											key={`${item.id}-${index}`}
+											className={style.orders_list_column_mySwiper_list_header_title}
+										>
+											<h3>{text}</h3>
+										</div>
+									))}
+							</div>
+							<div className={style.orders_list_column_mySwiper_list_body}>
+								{item.column &&
+									item.column.map((i) => (
+										<div
+											key={`column-${i.id}`}
+											className={style.orders_list_column_mySwiper_list_body_row}
+										>
+											<span>{i.text}</span>
+											<span>{i.orderId}</span>
+											<span>{i.type}</span>
+											<span>{i.userId}</span>
+											<span>{i.date}</span>
+											<span>{i.amount}</span>
+											<Link to="/orderDetails">
+												<button>{i.action}</button>
+											</Link>
+										</div>
+									))}
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className={style.orders_list_column_mySwiper_list}>
+							<div className={style.orders_list_column_mySwiper_list_header}>
+								{item.title &&
+									item.title.map((text, index) => (
+										<div
+											key={`${item.id}-${index}`}
+											className={style.orders_list_column_mySwiper_list_header_title}
+										>
+											<h3>{text}</h3>
+										</div>
+									))}
+							</div>
+							<div className={style.orders_list_column_mySwiper_list_body}>
+								{item.column &&
+									item.column.map((i) => (
+										<div
+											key={`column-${i.id}`}
+											className={style.orders_list_column_mySwiper_list_body_row}
+										>
+											<span>{i.text}</span>
+											<span>{i.orderId}</span>
+											<span>{i.type}</span>
+											<span>{i.userId}</span>
+											<span>{i.date}</span>
+											<span>{i.amount}</span>
+											<Link to="/orderDetails">
+												<button>{i.action}</button>
+											</Link>
+										</div>
+									))}
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className={style.orders_list_column_mySwiper_list}>
+							<div className={style.orders_list_column_mySwiper_list_header}>
+								{item.title &&
+									item.title.map((text, index) => (
+										<div
+											key={`${item.id}-${index}`}
+											className={style.orders_list_column_mySwiper_list_header_title}
+										>
+											<h3>{text}</h3>
+										</div>
+									))}
+							</div>
+							<div className={style.orders_list_column_mySwiper_list_body}>
+								{item.column &&
+									item.column.map((i) => (
+										<div
+											key={`column-${i.id}`}
+											className={style.orders_list_column_mySwiper_list_body_row}
+										>
+											<span>{i.text}</span>
+											<span>{i.orderId}</span>
+											<span>{i.type}</span>
+											<span>{i.userId}</span>
+											<span>{i.date}</span>
+											<span>{i.amount}</span>
+											<Link to="/orderDetails">
+												<button>{i.action}</button>
+											</Link>
+										</div>
+									))}
+							</div>
+						</SwiperSlide>
+					</div>
+        ))}
+      </Swiper>
+    </div>
+     <div className={style.orders_list_column_mySwiper_controls}>
+     	<button 
+		 		onClick={handlePrev} 
+				className={style.orders_list_column_mySwiper_controls_button}
+			>
+       <img src={left} alt="Previous" />
+       <span>Previous</span>
+     </button>
+			<div className={style.orders_list_column_mySwiper_controls_pagination}>
+				{pages.map((_, index) => (
+					<button 
+						style={isActive === index ? buttonStyles : undefined}
+						className={isActive === index
+							? style.orders_list_column_mySwiper_controls_pagination_button_active
+							: style.orders_list_column_mySwiper_controls_pagination_button
+						}
+					>  
+						<span
+							key={index}
+							onClick={() => goToSlide(index)}
+						>
+							{index + 1}
+						</span>
+					</button>
+					))}
+			</div>
+			<button 
+				onClick={handleNext} 
+				className={style.orders_list_column_mySwiper_controls_button}
+			>
+				<span>Next</span>
+				<img src={right} alt="Next" />
+			</button>
+   	</div>
+   </>
+  );
+}
